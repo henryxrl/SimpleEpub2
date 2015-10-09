eb52812067b23a0658f6c6385dd7239cfa0bcfea
henryxrl <henryxrl@gmail.com>
2015-10-09 14:05:10 -0400

    10/9/2015 - 2
    10/9/2015 Update:
    1. FIXED: Application cannot start if setting ini does not exist
    2. NEW: Switched from ILMerge to Costura.Fody
    3. NEW: Use GenerateUpdateXML to automatically write update XML file
    4. FIXED: GenerateUpdateXML bug fixes
    
    10/9/2015 更新：
    1. 修复：当设置文件不存在时无法打开应用程序
    2. 新增：不用ILMerge，改用Costura.Fody
    3. 新增：用GenerateUpdateXML来自动生成升级日志
    4. 新增：解决了GenerateUpdateXML的一些bugs

--- GenereateUpdateXML/Form1.cs
+++ GenereateUpdateXML/Form1.cs
@@ -2,24 +2,25 @@
 using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 using System.Xml;
 
 namespace GenereateUpdateXML
 {
     public partial class Form1 : DevComponents.DotNetBar.Metro.MetroForm
     {
-        static String xml_dir = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..");
-        static String xml_path = Path.Combine(xml_dir, @"SimpleEpub2_Update.xml");
-        static String exe_path = Path.Combine(xml_dir, @"SimpleEpub2\bin\Release\SimpleEpub2.exe");
-        static String exe_name = Path.GetFileName(exe_path);
-        static String exe_name_no_ext = Path.GetFileNameWithoutExtension(exe_path);
-        static String exe_URL = "https://raw.githubusercontent.com/henryxrl/SimpleEpub2/master/SimpleEpub2/bin/Release/SimpleEpub2.exe";
-        static String exe_MD5 = GetMd5(exe_path);
-        static String exe_ver = FileVersionInfo.GetVersionInfo(exe_path).ProductVersion;
+        private static String xml_dir = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..");
+        private static String xml_path = Path.Combine(xml_dir, @"SimpleEpub2_Update.xml");
+        private static String exe_path = Path.Combine(xml_dir, @"SimpleEpub2\bin\Release\SimpleEpub2.exe");
+        private static String exe_name = Path.GetFileName(exe_path);
+        private static String exe_name_no_ext = Path.GetFileNameWithoutExtension(exe_path);
+        private static String exe_URL = "https://raw.githubusercontent.com/henryxrl/SimpleEpub2/master/SimpleEpub2/bin/Release/SimpleEpub2.exe";
+        private static String exe_MD5 = GetMd5(exe_path);
+        private static String exe_ver = FileVersionInfo.GetVersionInfo(exe_path).ProductVersion;
 
-        XmlDocument doc = new XmlDocument();
-        XmlNode root;
-        XmlNode update;
+        private XmlDocument doc = new XmlDocument();
+        private XmlNode root;
+        private XmlNode update;
 
         public Form1()
         {
@@ -54,17 +55,17 @@ namespace GenereateUpdateXML
             if (log_en_array.Length == 0)
                 log_en = "";
             else
-                log_en = String.Join("\n\t", log_en_array);
+                log_en = String.Join("\n    ", log_en_array);
 
             var log_cn_array = textBox2.Lines;
             if (log_cn_array.Length == 0)
                 log_cn = "";
             else
-                log_cn = String.Join("\n\t", log_cn_array);
+                log_cn = String.Join("\n    ", log_cn_array);
 
             String date = DateTime.Today.ToString("d");
 
-            log = String.Concat(date, " Update:\n\t", log_en, "\n\n", date, " 更新：\n\t", log_cn);
+            log = String.Concat(date, " Update:\n    ", log_en, "\n\n", date, " 更新：\n    ", log_cn);
 
             appendNode("description", log);
             doc.Save(xml_path);
@@ -75,21 +76,17 @@ namespace GenereateUpdateXML
         static private String GetMd5(String path)
         {
             MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-
-            FileInfo fi = new FileInfo(path);
             FileStream stream = File.Open(path, FileMode.Open);
-
             md5.ComputeHash(stream);
-
             stream.Close();
 
-            string rtrn = "";
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < md5.Hash.Length; i++)
             {
-                rtrn += (md5.Hash[i].ToString("x2"));
+                sb.Append(md5.Hash[i].ToString("x2"));
             }
 
-            return rtrn.ToUpper();
+            return sb.ToString().ToUpper();
         }
 
         private void appendNode(String nodeName, String value)
