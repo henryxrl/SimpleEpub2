a20049c3b1d1219aa204c718ac381c06a56e2298
Henry Xu <henryxrl@gmail.com>
2020-07-16 00:06:46 -0400

    2020/7/16
    2020/7/16 Update:
        1. UPDATE: Update chapter title regex
    
    2020/7/16 更新：
        1. 更新：更新了标题正则表达式

--- SimpleEpub2/MainForm.cs
+++ SimpleEpub2/MainForm.cs
@@ -136,16 +136,27 @@ namespace SimpleEpub2
             {n,m}	重复n到m次
         */
         private static String regex_number = "——-——一二两三四五六七八九十○零百千壹贰叁肆伍陆柒捌玖拾佰仟0-9０-９";
-		private static String regex_titles_chinese = "[第序终終卷【]\\s*([" + regex_number + "\\s/\\、、]*)\\s*[章节節回集卷部】]";
-		private static String regex_1 = "^(\\s*([【])?(正文\\s*)?" + regex_titles_chinese + "\\s*$)";
-        private static String regex_2 = "^(\\s*([【])?(正文\\s*)?" + regex_titles_chinese + "\\s+.{1,50}$)";
-        private static String regex_other_titles = "内容简介|內容簡介|内容介绍|內容介紹|内容梗概|内容大意|小说简介|小說簡介|小说介绍|小說介紹|小说大意|小說大意|书籍简介|書籍簡介|书籍介绍|書籍介紹|书籍大意|書籍大意|作品简介|作品簡介|作品介绍|作品介紹|作品大意|作者简介|作者簡介|作者介绍|作者介紹|简介|簡介|介绍|介紹|大意|梗概|序|序言|序章|序幕|前言|楔子|引言|引子|终章|終章|大结局|结局|结尾|尾声|尾聲|后记|後記|完本|完本感言|完结|完结感言|出版后记|出版後記|谢辞|謝辭|番外|番外篇";
-        private static String regex_3 = "^(\\s*(" + regex_other_titles + ")\\s*$)";
-        private static String regex_4 = "^(\\s*(" + regex_other_titles + ")\\s+.{0,50}?$)";
-        private static String regex_titles_english = "chapter|appendix|appendices|preface|Foreword|Introduction|Prologue|Epigraph|Table of contents|Epilogue|Afterword|Conclusion|Glossary|Acknowledgments|Bibliography|Index|Errata|Colophon|Copyright";
-        private static String regex_5 = "^(\\s*((?i)" + regex_titles_english + ")\\s*$)";
-        private static String regex_6 = "^(\\s*((?i)" + regex_titles_english + ")\\s+.{0,50}?$)";
-        private static String regex = regex_1 + "|" + regex_2 + "|" + regex_3 + "|" + regex_4 + "|" + regex_5 + "|" + regex_6;
+		private static String regex_titles_chinese_pre_1 = "[第序终終卷【]\\s*([";
+		private static String regex_titles_chinese_post_1 = "\\s/\\、、]*)\\s*[章节節回集卷部篇】]";
+		private static String regex_titles_chinese_1 = regex_titles_chinese_pre_1 + regex_number + regex_titles_chinese_post_1;
+		private static String regex_1 = "^(\\s*([【])?(正文\\s*)?" + regex_titles_chinese_1 + "\\s*$)";
+        private static String regex_2 = "^(\\s*([【])?(正文\\s*)?" + regex_titles_chinese_1 + "\\s+.{1,50}$)";
+		
+		private static String regex_titles_chinese_pre_2 = "[序终終卷【]\\s*([";
+		private static String regex_titles_chinese_post_2 = "\\s/\\、、]*)\\s*";
+		private static String regex_titles_chinese_2 = regex_titles_chinese_pre_2 + regex_number + regex_titles_chinese_post_2;
+		private static String regex_3 = "^(\\s*([【])?(正文\\s*)?" + regex_titles_chinese_2 + "\\s*$)";
+        private static String regex_4 = "^(\\s*([【])?(正文\\s*)?" + regex_titles_chinese_2 + "\\s+.{1,50}$)";
+        
+		private static String regex_other_titles = "内容简介|內容簡介|内容介绍|內容介紹|内容梗概|内容大意|小说简介|小說簡介|小说介绍|小說介紹|小说大意|小說大意|书籍简介|書籍簡介|书籍介绍|書籍介紹|书籍大意|書籍大意|作品简介|作品簡介|作品介绍|作品介紹|作品大意|作者简介|作者簡介|作者介绍|作者介紹|简介|簡介|介绍|介紹|大意|梗概|序|序言|序章|序幕|前言|楔子|引言|引子|终章|終章|大结局|结局|结尾|尾声|尾聲|后记|後記|完本|完本感言|完结|完结感言|出版后记|出版後記|谢辞|謝辭|番外|番外篇";
+        private static String regex_5 = "^(\\s*(" + regex_other_titles + ")\\s*$)";
+        private static String regex_6 = "^(\\s*(" + regex_other_titles + ")\\s+.{0,50}?$)";
+        
+		private static String regex_titles_english = "chapter|appendix|appendices|preface|Foreword|Introduction|Prologue|Epigraph|Table of contents|Epilogue|Afterword|Conclusion|Glossary|Acknowledgments|Bibliography|Index|Errata|Colophon|Copyright";
+        private static String regex_7 = "^(\\s*((?i)" + regex_titles_english + ")\\s*$)";
+        private static String regex_8 = "^(\\s*((?i)" + regex_titles_english + ")\\s+.{0,50}?$)";
+        
+		private static String regex = regex_1 + "|" + regex_2 + "|" + regex_3 + "|" + regex_4 + "|" + regex_5 + "|" + regex_6 + "|" + regex_7 + "|" + regex_8;
 		private static String emptyLineRegex = "^\\s*$";
 
 		private List<String> bookAndAuthor = new List<String>();
@@ -3304,7 +3315,7 @@ namespace SimpleEpub2
 				int insert_br_idx = -1;
 				for (int i = 0; i < result.Length; ++i)
 				{
-					Regex r = new Regex("^(" + regex_titles_chinese + "$)|^(" + regex_other_titles + "$)");
+					Regex r = new Regex("^(" + regex_titles_chinese_1 + "$)|^(" + regex_titles_chinese_2 + "$)|^(" + regex_other_titles + "$)");
 					Match m = r.Match(result[i]);
 					if (m.Success)
 					{
